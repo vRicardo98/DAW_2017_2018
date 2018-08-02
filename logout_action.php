dcd7674936ebfdcd85ae9031d0ce4cc6af453377
vRicardo98 <41165271+vRicardo98@users.noreply.github.com>
2018-08-02 11:20:09 +0100

    Add files via upload

--- logout_action.php
+++ logout_action.php
@@ -4,6 +4,19 @@ unset($_SESSION);
 session_destroy();
 setcookie("user_id", null, time() - 3600);
 
-// Carrega o template message_template.html
-header("Location: message_template.html");
+require_once "inc/IT.php";
+//require_once 'db.php';
+
+// Cria um novo objecto template
+$template = new HTML_Template_IT('.');
+
+// Carrega o template Filmes2_TemplateIT.html
+$template->loadTemplatefile('message_template.html', true, true);
+
+//MENU
+$template->setCurrentBlock("MAIN");
+$template->setVariable('MESSAGE', "See you back soon!");
+// Faz o parse do bloco MENU
+$template->parseCurrentBlock();
+$template->show();
 ?>
