79fb7dac0da67f58e8113804b0625a54d7186d70
vRicardo98 <41165271+vRicardo98@users.noreply.github.com>
2018-08-02 11:53:17 +0100

    Add files via upload

--- new_password_action.php
+++ new_password_action.php
@@ -6,6 +6,8 @@ require_once 'db.php';
 $db = dbconnect($hostname,$db_name,$db_user,$db_passwd); 
 
 $email = $_POST['email'];
+$password = $_POST['password'];
+$repassword = $_POST['repassword'];
 
 if($db) {	
 	// criar query numa string
@@ -19,18 +21,27 @@ if($db) {
 	$nrows  = mysql_num_rows($result);
 	
 	if(isset($_GET["TOKEN"]) && ((time()*3600)-$row[reset_digest])<=1) {
-		$query  = "UPDATE users SET password = '" . $_GET["TOKEN"]) . "' WHERE email = '" . $row["email"] . "'";
+		if($password == $repassword) {
+			$query  = "UPDATE users SET password = '" . $password . "' WHERE email = '" . $row["email"] . "'";
 			
-		// executar a query
-		if(!($result = @ mysql_query($query,$db))) {
-			showerror();
+			// executar a query
+			if(!($result = @ mysql_query($query,$db))) {
+				showerror();
+			}
+							
+			$template->setCurrentBlock("FORM-MESSAGE");
+			$template->setVariable('MESSAGE', '<div class="form-message">' . 'Password reset successfully!' . '</div>');
+			$template->parseCurrentBlock();
+			
+			header("Location:message.php?code=2");
 		}
-						
-		$template->setCurrentBlock("FORM-MESSAGE");
-		$template->setVariable('MESSAGE', '<div class="form-message">' . 'Password reset successfully!' . '</div>');
-		$template->parseCurrentBlock();
-		
-		header("Location:message.php?code=2");
+		else {
+			$template->setCurrentBlock("FORM-MESSAGE");
+			$template->setVariable('MESSAGE', '<div class="form-message">' . 'Password confirmation does not match!' . '</div>');
+			$template->parseCurrentBlock();
+			
+			header("Location:message.php?code=1");
+		}
 	}
 	else {
 		$template->setCurrentBlock("FORM-MESSAGE");
